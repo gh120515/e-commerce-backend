f70983a07e82e7b9f57f6d42a41f1dae6035bc30
gh120515 <123872197+gh120515@users.noreply.github.com>
2023-06-14 21:16:09 +0930

    category routed

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -60,6 +60,13 @@ router.put('/:id', async (req, res) => {
       },
     });
 
+    if (!tagData) {
+      res.status(404).json({ message: 'No tag found with that id!' });
+      return;
+    }
+
+    res.status(200).json(tagData);
+
   } catch (err) {
     res.status(500).json(err);
   }
